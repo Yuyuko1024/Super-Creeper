e75dd20055dd46dc3f89ae1e5efffdea0898e0d0
Yuyuko1024 <dingwenxuan4@gmail.com>
2025-06-27 15:09:19 +0800

    feat: 添加玩家近战时瞬爆机制

--- src/main/java/First/creeper_super/CreeperDeath.java
+++ src/main/java/First/creeper_super/CreeperDeath.java
@@ -4,6 +4,7 @@ package First.creeper_super;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -18,6 +19,17 @@ public class CreeperDeath {
         DamageSource damageSource = livingHurtEvent.getSource();
         Level level = livingEntity.level();
         if (!level.isClientSide() && livingEntity instanceof Creeper creeper) {
+            // 检查是否是玩家或者生物造成的伤害
+            if (damageSource.getEntity() instanceof LivingEntity attacker) {
+                // 判断是否为近战伤害
+                if (isDirectDamage(damageSource)) {
+                    // 如果是近战伤害，则直接瞬爆
+                    creeper.setSwellDir(3);
+                    creeper.getPersistentData().putBoolean("Boom", true);
+                }
+            }
+
+
             if ((damage > creeper.getHealth() && creeper.getSwellDir() > 0)) {
                 creeper.heal(1);
                 creeper.setSwellDir(30);
@@ -39,6 +51,13 @@ public class CreeperDeath {
                 }
             }
         }
+    }
+
+    // 检查是否为直接伤害
+    private static boolean isDirectDamage(DamageSource damageSource) {
+        return damageSource.is(DamageTypes.PLAYER_ATTACK) ||
+                damageSource.is(DamageTypes.MOB_ATTACK) ||
+                (damageSource.getDirectEntity() == damageSource.getEntity());
     }
 
 }
